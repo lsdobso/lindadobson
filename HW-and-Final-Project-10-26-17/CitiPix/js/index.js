b9a7cf4291ccb98c44c97bb9dc5ab840e6584147
Linda <linda.dobson@emory.edu>
2017-10-26 18:54:35 -0400

    HW and Wireframe Updated

--- HW-and-Final-Project-10-26-17/CitiPix/js/index.js
+++ HW-and-Final-Project-10-26-17/CitiPix/js/index.js
@@ -1,4 +1,7 @@
-
+// Psuedo Code notes
+// Use user's input to change background corresponding to the city the user inputs by using JQuery
+// Make the script wait for when the document is ready
+// Make page not reload and make form wait for submit
 
  	$(document).ready(function(){
  	$('form').submit(addCityName);
@@ -10,9 +13,9 @@
 
  		if (city === 'New York' || city === 'NYC' || city === 'New York City' || city === 'new york' || city === 'nyc' || city === 'new york city') {
  			$('body').removeClass('starter-image').addClass('nyc');
- 		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area' || city === 'san francisco' || city === 'sf' || city === 'bay area') {
+ 		} else if (city === 'San Francisco' || city === 'SF' || city === 'Bay Area' || city === 'san francisco' || city === 'sf') {
  			$('body').removeClass('starter-image').addClass('sf');
- 		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX' || city === 'los angeles' || city === 'la' || city === 'lax') {
+ 		} else if (city === 'Los Angeles' || city === 'LA' || city === 'LAX' || city === 'los angeles' || city === 'la') {
  			$('body').removeClass('starter-image').addClass('la');
  		} else if (city === 'Austin' || city === 'ATX' || city === 'austin' || city === 'atx') {
  			$('body').removeClass('starter-image').addClass('austin');
